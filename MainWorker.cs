b96414f94fa3c74e5bd3004e77d7a1ad424bbd27
dr <dr@dr>
2021-05-04 01:11:11 -0700

    Working prototype (hard-coded F11) both spams clicks and suppresses the F11 press itself. Gating to about 32 clicks per second for now (though can easily reach hundreds of clicks per second against software equipped to detect mouse changes that fast).

--- MainWorker.cs
+++ MainWorker.cs
@@ -1,5 +1,5 @@
 ﻿using LowLevelInput.Hooks;
-using System;
+using LowLevelInput.WindowsHooks;
 using System.Threading;
 
 namespace Clicknado
@@ -8,6 +8,7 @@ namespace Clicknado
     {
         private static Thread workerThread;
         private static LowLevelKeyboardHook keyboardHook;
+        private static readonly bool[] keysDown = new bool[255];
 
         public static bool IsEnabled { get; private set; } = false;
 
@@ -40,19 +41,38 @@ namespace Clicknado
 
         private static void DoWork()
         {
+            WindowsHookFilter.Filter += (VirtualKeyCode key, KeyState state) => {
+                if (key == VirtualKeyCode.F11)
+                {
+                    // Filter the event (blocking it from being handled by other programs) but still mark the key as down/up accordingly.
+                    OnKeyboardEvent(key, state);
+                    return true;
+                }
+                return false;
+            };
+            
             keyboardHook = new LowLevelKeyboardHook();
             keyboardHook.OnKeyboardEvent += OnKeyboardEvent;
             keyboardHook.InstallHook();
 
             while (IsEnabled)
             {
-                Thread.Sleep(11);
+                Triggers.Handle(keysDown);
+                Thread.Sleep(1);
             }
         }
 
         private static void OnKeyboardEvent(VirtualKeyCode key, KeyState state)
         {
-            Console.WriteLine($"{key} {state}");
+            // We need this to be fast, because some Windows are really picky about low level hook registrations taking too much time.
+            // For a guaranteed fast per-key reaction, we want to avoid locks and thus fancy data structures with locking concerns.
+            // As such, we're going to just maintain a small array of keys with boolean down state for reading in another thread, as we
+            // can safely switch the boolean value without causing issues with the other thread.
+            int index = (int)key;
+            if (index >= 0 && index < 255)
+            {
+                keysDown[index] = state == KeyState.Down;
+            }
         }
     }
 }
--- Triggers.cs
+++ Triggers.cs
@@ -1,6 +1,8 @@
 ﻿using LowLevelInput.Hooks;
 using System;
+using System.Linq;
 using System.Collections.Generic;
+using SimWinInput;
 
 namespace Clicknado
 {
@@ -25,6 +27,8 @@ namespace Clicknado
     public static class Triggers
     {
         private static readonly List<Trigger> triggers = new List<Trigger>();
+        private static bool lastRunHandledMouseDown = false;
+        private static bool leftMouseHeldDown = false;
 
         static Triggers()
         {
@@ -34,11 +38,47 @@ namespace Clicknado
                 KeyCodes = new List<VirtualKeyCode>() { VirtualKeyCode.F11 },
                 TriggerAction = new TriggerAction()
                 {
-                    ActionDelayMS = 100,
+                    ActionDelayMS = 10,
                     LastActionTime = DateTime.Now
                 }
             });
         }
 
+        // TODO: Also a HandleToggle for keys to be registered as TOGGLES instead of just "HOLD-TO-SPAM"?  Enum for TriggerType perhaps?
+        public static void Handle(bool[] keysDown)
+        {
+            // For simplicity, each time we run will either be a mouse-down pass, or a mouse-up pass. So the fastest we can try to spam
+            // is roughly cycles of 1 ms held down, 1 ms held up.
+            if (lastRunHandledMouseDown)
+            {
+                // Determine whether mouse is held down, in order to trigger the mouse back up.
+                if (leftMouseHeldDown)
+                {
+                    InteropMouse.mouse_event((uint)InteropMouse.MouseEventFlags.LeftUp, 0, 0, 0, 0);
+                    leftMouseHeldDown = false;
+                }
+                lastRunHandledMouseDown = false;
+            }
+            else
+            {
+                // Determine whether left mouse down should occur, based on at least one registered key combo being held.
+                var now = DateTime.Now;
+                var reactions = from t in triggers
+                                let reaction = t.TriggerAction
+                                where t.KeyCodes.All(key => keysDown[(int)key]) &&
+                                      reaction.LastActionTime.AddMilliseconds(reaction.ActionDelayMS) <= now
+                                select reaction;
+                if (reactions.Any())
+                {
+                    foreach (var reaction in reactions)
+                    {
+                        reaction.LastActionTime = DateTime.Now;
+                    }
+                    InteropMouse.mouse_event((uint)InteropMouse.MouseEventFlags.LeftDown, 0, 0, 0, 0);
+                    leftMouseHeldDown = true;
+                }
+                lastRunHandledMouseDown = true;
+            }
+        }
     }
 }
